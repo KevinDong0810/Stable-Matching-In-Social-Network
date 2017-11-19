ee3b27f281b2f0776f73247549dbacc0af292804
KevinDong0810 <dongk@buaa.edu.cn>
2017-11-19 14:44:04 +0800

    update the crawler programm

--- EmpiricalStudy/CrawlerProgram/co-author.py
+++ EmpiricalStudy/CrawlerProgram/co-author.py
@@ -1,7 +1,7 @@
 #coding=utf-8
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains #引入ActionChains鼠标操作类  
-from selenium.webdriver.common.keys import Keys #引入keys类操作  
+from selenium import webdriver              #use selenium to imitate the crawler process
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time  
 import xlrd
 import xlwt
@@ -10,11 +10,8 @@ import socket
 import string
 socket.setdefaulttimeout(100)
 
-def s(int):  
-    time.sleep(int)
-
-def getInfo(proname, url,table):
-    try:
+def getInfo(proname, url,table):  #get the specific information of the co-authors
+    try:                          #try to get to the homepage of each professor, and click the button to view their co-authors
         browser.get(url)
         browser.find_element_by_class_name("ga-top-coauthors-view-all").click()
         time.sleep(3)
@@ -23,78 +20,74 @@ def getInfo(proname, url,table):
     global count
     #get information
     for i in range(50):
-        st = "//ul/li["+str(i+1)+"]"+"[@class='nova-e-list__item']"
-        #print(st)
+        st = "//ul/li["+str(i+1)+"]"+"[@class='nova-e-list__item']"    #use xpath to locate the information of co-authors
         try:
             person = browser.find_element_by_xpath(st)
         except:
             return
-        if "Not" in person.text:
+        if "Not" in person.text:                                       #not means the co-author has no researchgate account
             break
-        sp = person.text.split("\n",2)
-        if (len(sp)==1):
+        sp = person.text.split("\n",2)                                 #split the information
+        if (len(sp)==1):                                               #if there is nothing, skip
             continue
-        name = sp[0]
-        sp1 = sp[1].split("(",1)
-        sp2 = sp1[1].split(")",1)
+        name = sp[0]                                                   #name
+        sp1 = sp[1].split("(",1)                                       #score
+        sp2 = sp1[1].split(")",1)                                      #publication
         score = sp1[0]
         pub = sp2[0]
-        school = sp2[1]
+        school = sp2[1]                                                #school
         count += 1
-        table.write(count,0, proname)
+        table.write(count,0, proname)                                  #write the information to excel
         table.write(count,1, name)
         table.write(count,2, score)
         table.write(count,3, pub)
         table.write(count,4, school)
 
-def getName(proname, url,table):
+def getName(proname, url,table):                                        #get names of these professors' co-authors
     try:
-        browser.get(url)
-        browser.find_element_by_class_name("ga-top-coauthors-view-all").click()
+        browser.get(url)                                                            #get to the homepage of the professor
+        browser.find_element_by_class_name("ga-top-coauthors-view-all").click()     #click the button to open the list of co-authors
         time.sleep(5)
     except:
         print("not find")
         return
     global count
     #get Name
-    person = browser.find_elements_by_class_name('nova-v-person-list-item__title--clamp-1')
+    person = browser.find_elements_by_class_name('nova-v-person-list-item__title--clamp-1')             #use the classname to locate the name
     time.sleep(3)
-    paper = browser.find_elements_by_class_name('profile-coauthors-account-item__shared-publications')
+    paper = browser.find_elements_by_class_name('profile-coauthors-account-item__shared-publications')  #use the classname to locate the paper
     for i in range(50):
         try:
-            name = person[i].text
-            num = paper[i].text
+            name = person[i].text                       #get the name of the co-author
+            num = paper[i].text                         #get the number of papers they cooperate
             if ((name == person[0].text)&(i != 0)):
                 return
             print(num)
-            num = num.replace("(", "")
+            num = num.replace("(", "")                  #remove he bracket of the number of papers
             num = num.replace(")", "")
             print(num)
             count += 1
-            table.write(count,0, proname)
+            table.write(count,0, proname)               #write the name and the number to the excel
             table.write(count,1, name) 
             table.write(count,2,num)
         except:
             return
 
-browser = webdriver.Chrome()  
-browser.get('https://www.researchgate.net/login')  
-browser.find_element_by_id('input-login').send_keys(u'14041153@buaa.edu.cn')
-browser.find_element_by_id('input-password').send_keys(u'a19960810')
-browser.find_element_by_xpath("//form/button").click()
+browser = webdriver.Chrome()                                             #open a chrome browser
+browser.get('https://www.researchgate.net/login')                        #get to the login page
+browser.find_element_by_id('input-login').send_keys(u'username')         #input username
+browser.find_element_by_id('input-password').send_keys(u'password')      #input password
+browser.find_element_by_xpath("//form/button").click()                   #click the button to log in
 time.sleep(1)
-data = xlrd.open_workbook('core_member.xlsx')
+data = xlrd.open_workbook('core_member.xlsx')                           #open the excel of core members
 table = data.sheets()[0]
-#proname = table.col_values(0)
-#url = table.col_values(4)
-rb = xlrd.open_workbook('co-author.xls') 
-wb = xlutils.copy.copy(rb)
-#获取sheet对象，通过sheet_by_index()获取的sheet对象没有write()方法
-ws = wb.get_sheet(0)
+rb = xlrd.open_workbook('co-author.xls')                                #open the excel of co-authors
+wb = xlutils.copy.copy(rb)                                              #continue to write instead of replacing
+ws = wb.get_sheet(0)                                                    #get the sheet to write
 count = 0
 nrows = table.nrows
-for i in range(nrows):
-    name = table.col(0)[i].value
-    url_i = str(table.col(5)[i].value)
-    getName(name,url_i,ws)
+for i in range(nrows):                                                  #go through all the rows
+    name = table.col(0)[i].value                                        #get the name of the professor
+    url_i = str(table.col(5)[i].value)                                  #get the homepage of the professor
+    getName(name,url_i,ws)                                              #get names and number of paper of the professor's co-authors
     wb.save('co-author.xls')
--- EmpiricalStudy/CrawlerProgram/follower.py
+++ EmpiricalStudy/CrawlerProgram/follower.py
@@ -1,53 +1,52 @@
 #coding=utf-8
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains #引入ActionChains鼠标操作类  
-from selenium.webdriver.common.keys import Keys #引入keys类操作  
+from selenium import webdriver  #use selenium to imitate the crawler process
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time  
 import xlrd
 import xlwt
 import xlutils.copy
 import socket
-socket.setdefaulttimeout(100)
+socket.setdefaulttimeout(100) #set timeout to 100s, to prevent process from running too long
 
 def s(int):  
     time.sleep(int)
     
-def getName(name,url,table):
-    url = url+"/info"
-    browser.get(url)
+def getName(name,url,table):#get names of each professor's following people
+    url = url+"/info"   #info web has more clear information
+    browser.get(url)    #get to the url
     try:
-        browser.find_element_by_class_name("ga-view-all-profile-followers").click()    
-        time.sleep(3)
-        follower = browser.find_elements_by_class_name("display-name")
+        browser.find_element_by_class_name("ga-view-all-profile-followers").click()    #click the button to open the list of followers
+        time.sleep(3)   #sleep 3s to wait for the click delay, in case the list doesn't open
+        follower = browser.find_elements_by_class_name("display-name")  #find names of the followers
         global count
         for i in range(60):
             try:
                 print(follower[i].text)
                 count = count+1
-                table.write(count,0, name)
+                table.write(count,0, name)               #write names to excel
                 table.write(count,1,follower[i].text)
-            except:
+            except:                                      #if there is some mistake(such as the number of names is less than 150), do not terminate and just ignore,
                 return
     except:
         return
 
-browser = webdriver.Chrome()  
-browser.get('https://www.researchgate.net/login')  
-browser.find_element_by_id('input-login').send_keys(u'zfone@buaa.edu.cn')  
-browser.find_element_by_id('input-password').send_keys(u'zfone533210') 
-browser.find_element_by_xpath("//form/button").click()
-time.sleep(1)
+browser = webdriver.Chrome()                #open a chrome browser
+browser.get('https://www.researchgate.net/login')  #get to login page
+browser.find_element_by_id('input-login').send_keys(u'username') #input the username
+browser.find_element_by_id('input-password').send_keys(u'password')  #input the password
+browser.find_element_by_xpath("//form/button").click()  #click the button to login in
+time.sleep(1)                               #sleep 1 time to wait for the network delay
 
-data = xlrd.open_workbook('member_1.xlsx')
+data = xlrd.open_workbook('member_1.xlsx')  #open the excel of these people need to get their followers
 table = data.sheets()[0]
-rb = xlrd.open_workbook('follower_1.xls')
-wb = xlutils.copy.copy(rb)
-#获取sheet对象，通过sheet_by_index()获取的sheet对象没有write()方法
-ws = wb.get_sheet(0)
+rb = xlrd.open_workbook('follower_1.xls')   #open the excel of followers
+wb = xlutils.copy.copy(rb)                  #continue to write instead of replacing
+ws = wb.get_sheet(0)                        #get the sheet
 count = 0
-for i in range(table.nrows):
-    name = table.col(0)[i].value
-    url_i = str(table.col(5)[i].value)
-    getName(name,url_i,ws)
+for i in range(table.nrows):                #go through all the rows
+    name = table.col(0)[i].value            #get the name of the professor
+    url_i = str(table.col(5)[i].value)      #get the homepage of the professor
+    getName(name,url_i,ws)                  #get names of his followers
     wb.save('follower_1.xls')
 
